d7fe8ad6a9eab8a7475be78bf761db6f339913d5
Kateryna <kporhun@gmail.com>
2019-05-09 20:59:29 +0300

    add a lot of files and ways for them

--- src/App/Header/Header.js
+++ src/App/Header/Header.js
@@ -1,40 +1,32 @@
 import React from 'react'
 
-import './header.css'
-import './menu.css'
-import './cart.css'
 
+
+import './header.css'
+
+
+import Logo from './Logo/Logo'
+import Menu from './Menu/Menu'
+import Cart from './Cart/Cart'
 const Header= () => {
     return (
-        <div>
-                <header className="header">
-		<div className="container">
-			<div className="row">
-				<div className="col-lg-3">
-					<div className="logo">
-						<img src="img/logo.png" alt="" />
+		<div>
+			<header className="header">
+				<div className="container">
+					<div className="row">
+						<div className="col-lg-3">
+							<Logo/>
+						</div>
+						<div className="col-lg-6">
+							<Menu/>
+						</div>
+						<div className="col-lg-3">
+							<Cart/>
+						</div>
 					</div>
 				</div>
-				<div className="col-lg-6">
-					<div className="menu">
-						<ul>
-							<li><a href="">Home</a></li>
-							<li><a href="">Product</a></li>
-							<li><a href="">Payment</a></li>
-							<li><a href="">Shipping</a></li>
-						</ul>
-					</div>
-				</div>
-				<div className="col-lg-3">
-					<div className="cart text-center">
-						<div className="products-count">0</div>
-						<div className="products-price">$500</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</header>
-        </div> 
+			</header>
+		</div> 
     )
 }
 
