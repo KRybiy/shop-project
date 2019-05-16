329048aabb0511183d8d61c21eb9970029a6dbf9
Kateryna <kporhun@gmail.com>
2019-05-16 19:24:39 +0300

    use 'destruction' for product in ProductList

--- src/App/Main/Products/ProductsList.js
+++ src/App/Main/Products/ProductsList.js
@@ -8,14 +8,21 @@ const ProductList = () => {
             <h1 className="page-title"> Products List </h1>
             <div className="row"> 
                {
-                   products.map((product) => (
-                        <div className="col-lg-6" key={product.id}>
+                   products.map(({
+                    id,
+                    name,
+                    description,
+                    type,
+                    capacity,
+                    price,
+                   }) => (
+                        <div className="col-lg-6" key={id}>
                             <ProductListItem
-                            name={product.name}
-                            description={product.description}
-                            type={product.type}
-                            capacity={product.capacity}
-                            price={product.price}
+                            name={name}
+                            description={description}
+                            type={type}
+                            capacity={capacity}
+                            price={price}
                             />
                         </div>
                        ))
