72f76ed421f31f78863c5c78e6a89b92d64c2569
Kateryna <kporhun@gmail.com>
2019-05-21 21:21:49 +0300

    rewrite App to statefull component

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import '../common/style/reset.css'
 import '../common/style/base.css'
 
@@ -7,17 +7,24 @@ import Header from './Header/Header'
 import Main from './Main/Main'
 import Footer from './Footer/Footer'
 
+class App extends Component {
 
-const App= () => {
-    return (
-        <div>
-        <Header/>
-        <Main/>
-        <Footer/>
-         </div>
-    )
+    state ={
+        cartData: {
+            price:0,
+            totalCount:0,
+        }
+    }
+    render () {
+        return (
+            <div>
+            <Header/>
+            <Main/>
+            <Footer/>
+             </div>
+        )
+    }
 }
-
 
 
 export default App
