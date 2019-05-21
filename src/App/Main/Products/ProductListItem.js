27d80473fd57358bb0324664dba2d58c20b7588f
Kateryna <kporhun@gmail.com>
2019-05-21 20:25:06 +0300

    add arrow function onClick

--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react'
-//import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import './ProductListItem.css'
 
 class ProductListItem extends Component {
+        state = {
+            productCount:1 
+        }
+        onIncrementClick(){
+            this.setState((prevState)=>({
+                productCount:prevState.productCount+1
+            }))
+        }
+        static propTypes = {
+            name:PropTypes.string.isRequired,
+            description:PropTypes.string,
+            price:PropTypes.number,
+            type:PropTypes.string,
+            capacity:PropTypes.number,
+        }
     render () {
+        const {
+            name,
+            description = "No description...",
+            image,
+            type,
+            capacity,
+            price,
+        } = this.props
         return (
+           
             <div className="product-list-item">
                 <div className="product-img">
-                    <img src={this.props.image} alt=""/>
+                    <img src={image} alt=""/>
                 </div>
-                <h2 className="product-title"> {this.props.name}</h2>
-                <div className="product-description"> {this.props.description}</div>
-                <div className="product-features"> Type: {this.props.type} </div>
-                <div className="product-features"> Capacity: {this.props.capacity} Gb</div>
+                <h2 className="product-title"> {name}</h2>
+                <div className="product-description"> {description}</div>
+                <div className="product-features"> Type: {type} </div>
+                <div className="product-features"> Capacity: {capacity} Gb</div>
                 <div className="product-quantity"> 
                     <button>-</button>
-                    <input type="text" value="1"/>
-                    <button>+</button>
+                    <input type="text" value={this.state.productCount} readOnly/>
+                    <button
+                        onClick={()=>this.onIncrementClick()}
+                    >+</button>
                 </div>
-                <div className="product-price"> {this.props.price}$</div>
+                <div className="product-price"> {price}$</div>
                 <button className="btn-add-to-cart"> Add to Cart </button>
             </div>
         )
@@ -26,12 +52,12 @@ class ProductListItem extends Component {
 }
 
 
-        // ProductListItem.propTypes = {
-        //     name:PropTypes.string.isRequired,
-        //     image:PropTypes.string,
-        //     description:PropTypes.string,
-        //     type:PropTypes.string.isRequired,
-        //     capacity:PropTypes.number.isRequired,
-        //     price:PropTypes.number.isRequired,
-        // }
+        ProductListItem.propTypes = {
+         name:PropTypes.string.isRequired,
+        image:PropTypes.string,
+        description:PropTypes.string,
+        type:PropTypes.string.isRequired,
+         capacity:PropTypes.number.isRequired,
+        price:PropTypes.number.isRequired,
+        }
 export default ProductListItem
