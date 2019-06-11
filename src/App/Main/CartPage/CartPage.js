08560b8e0698ffbe2113ba7d44a60811836f16a2
Kateryna <kporhun@gmail.com>
2019-06-11 20:30:33 +0300

    added CartProductListItemExtended and styles for it

--- src/App/Main/CartPage/CartPage.js
+++ src/App/Main/CartPage/CartPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CartTotal from "../../../features/Cart/CartTotal"
 import CartProductList from "../../../features/Cart/CartProductList"
+import CartProductListItemExtended from "../../../features/Cart/CartProductListItemExtended"
 
 const CartPage = ({
     productsInCart,
@@ -8,9 +9,11 @@ const CartPage = ({
     return (
         <div>
             <h1 className="page-title"> Cart Page</h1>
+            
             <div className="cart text-center">
                 <CartProductList 
                 productsInCart={productsInCart}
+                CartListItemComponent={CartProductListItemExtended}
                 />
                 <div>
                   <CartTotal
--- src/features/Cart/CartProductsListItemExtended.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CartProductListItemExtended = () => {
-    return (
-
-    )
-}
-
-export default CartProductListItemExtended
